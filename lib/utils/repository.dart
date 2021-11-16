227ebae58575ebfbbdb87c6ea3ef3ad6022c1999
Thai Nghia Huynh <shipangei00@gmail.com>
2021-11-16 17:17:05 +0700

    Repository: minor fixes

--- lib/utils/repository.dart
+++ lib/utils/repository.dart
@@ -10,7 +10,7 @@ class Repository {
   Future<void> login(String userName, String password) =>
       apiService.login(userName, password);
 
-  Future<void> refresh(String userName) => apiService.refresh();
+  Future<void> refresh() => apiService.refresh();
 
-  Future<void> logout(String userName) => apiService.logout();
+  Future<void> logout() => apiService.logout();
 }
